bcb2d3e7e230414bcb30612bf7dbb9eb8e03bc17
francisnnumbi <francis@smirl.org>
2023-07-10 00:22:40 +0200

    update testing tuto

--- test/news_page_test.dart
+++ test/news_page_test.dart
@@ -7,9 +7,12 @@ import 'package:testing_flutter_apps_tuto/news_service.dart';
 class MockNewsService extends GetxController with Mock implements NewsService {}
 
     void main(){
-        late MockNewsService mockNewsService = MockNewsService();
-     
-        group('', (){});
+        late MockNewsService mockNewsService;
+        setUp(() {
+            mockNewsService = MockNewsService();
+        });
+
+        testWidgets('', ()async{});
 
       
     }
